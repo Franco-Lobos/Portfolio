595d5de03d021a46bf21be4cda79fcae0ecc8a25
Franco-Lobos <francolobos3@gmail.com>
2023-01-15 20:14:03 +0100

    oldTvLOADER

---
+++ portfolio/src/components/Canvas.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Sketch from "react-p5";
+
+const Canvas = ()=>{
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    let frameRate = 100;
+    let x = 1;
+    let y = 1;
+    let originW = w/2;
+    let originH = h/2;
+    let line = 12;
+    let pixel = 10;
+    let ready = 0;
+
+	const setup = (p5, canvasParentRef) => {
+		p5.createCanvas(w, h).parent(canvasParentRef);
+        p5.frameRate(frameRate);
+	};
+
+	const draw = (p5) => {
+		p5.background('#242424');
+
+        let ob1 = [originW-x, originH-y];
+        let ob2 = [originW+x, originH-y];
+        let ob3 = [originW+x, originH+y];
+        let ob4 = [originW-x, originH+y];
+
+        if(x<=originW){
+            x+=x*2;
+        }else{
+            if(y<=originH){
+                y+= y*2;
+            }
+            else{
+                ready= 1;
+            }
+        }
+
+        if(line>=2){
+            line--;
+        }
+        p5.fill('#fff');
+        p5.stroke('#fff');
+        p5.strokeWeight(line);
+        p5.quad(ob1[0],ob1[1],ob2[0],ob2[1],ob3[0],ob3[1],ob4[0],ob4[1]);
+        if(p5.frameCount<=60){
+            for(let i= 0; i<=w; i+=pixel){
+                for(let j= 0; j<=h; j+=pixel){
+                    let pix1 = [i, j];
+                    let pix2 = [i+pixel, j];
+                    let pix3 = [i+pixel, j+pixel];
+                    let pix4 = [i, j+pixel];
+                    let random = Math.floor(Math.random()*10)%2;
+                    p5.fill(random ? '#323232' : '#f0f0f0');
+                    p5.noStroke();
+                    p5.quad(pix1[0],pix1[1],pix2[0],pix2[1],pix3[0],pix3[1],pix4[0],pix4[1]);
+                }
+            }
+        }
+
+
+	};
+
+	return <Sketch setup={setup} draw={draw}/>;
+
+};
+
+export default Canvas;
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 import { ProfileConst } from "../constants/ProfileConst";
+import Canvas from "./Canvas";
+
 
 const Home =()=>{
     const [Profile, setProfile] =  useState(0);
@@ -14,6 +16,7 @@ const Home =()=>{
             <div>
                 {Profile.name + ' ' + Profile.lastName}
             </div>
+            <Canvas></Canvas>
         </div>
     );
 }
