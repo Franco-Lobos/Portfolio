18f3356da2ea1eb44a7d9296f4674806275742ea
Franco-Lobos <francolobos3@gmail.com>
2023-01-15 21:07:31 +0100

    shutDownAnination

--- portfolio/src/components/Canvas.jsx
+++ portfolio/src/components/Canvas.jsx
@@ -27,37 +27,59 @@ const Canvas = ()=>{
         let ob3 = [originW+x, originH+y];
         let ob4 = [originW-x, originH+y];
 
-        if(x<=originW){
-            x+=x*2;
-        }else{
-            if(y<=originH){
-                y+= y*2;
-            }
-            else{
-                ready= 1;
-            }
-        }
-
-        if(line>=2){
-            line--;
-        }
-        p5.fill('#fff');
-        p5.stroke('#fff');
-        p5.strokeWeight(line);
-        p5.quad(ob1[0],ob1[1],ob2[0],ob2[1],ob3[0],ob3[1],ob4[0],ob4[1]);
-        if(p5.frameCount<=60){
-            for(let i= 0; i<=w; i+=pixel){
-                for(let j= 0; j<=h; j+=pixel){
-                    let pix1 = [i, j];
-                    let pix2 = [i+pixel, j];
-                    let pix3 = [i+pixel, j+pixel];
-                    let pix4 = [i, j+pixel];
-                    let random = Math.floor(Math.random()*10)%2;
-                    p5.fill(random ? '#323232' : '#f0f0f0');
-                    p5.noStroke();
-                    p5.quad(pix1[0],pix1[1],pix2[0],pix2[1],pix3[0],pix3[1],pix4[0],pix4[1]);
+        if(!ready){
+            if(x<=originW){
+                x+=x*2;
+            }else{
+                if(y<=originH){
+                    y+= y*2;
+                }
+                else{
+                    ready= 1;
                 }
             }
+    
+            if(line>=2){
+                line--;
+            }
+            p5.fill('#fff');
+            p5.stroke('#fff');
+            p5.strokeWeight(line);
+            p5.quad(ob1[0],ob1[1],ob2[0],ob2[1],ob3[0],ob3[1],ob4[0],ob4[1]);
+        }
+
+        else{
+            if(p5.frameCount<=60){
+                for(let i= 0; i<=w; i+=pixel){
+                    for(let j= 0; j<=h; j+=pixel){
+                        let pix1 = [i, j];
+                        let pix2 = [i+pixel, j];
+                        let pix3 = [i+pixel, j+pixel];
+                        let pix4 = [i, j+pixel];
+                        let random = Math.floor(Math.random()*10)%2;
+                        p5.fill(random ? '#323232' : '#f0f0f0');
+                        p5.noStroke();
+                        p5.quad(pix1[0],pix1[1],pix2[0],pix2[1],pix3[0],pix3[1],pix4[0],pix4[1]);
+                    }
+                }
+            }
+            else{
+                y-=p5.frameCount*1.6;
+                if(y>=0){
+                    p5.fill('#fff');
+                    p5.stroke('#fff');
+                    p5.strokeWeight(line);
+                }
+                else{
+                    p5.noFill();
+                    p5.noStroke();
+                    p5.frameRate(0);
+                }
+         
+                p5.quad(ob1[0],ob1[1],ob2[0],ob2[1],ob3[0],ob3[1],ob4[0],ob4[1]);
+            }
+
+           
         }
 
 
