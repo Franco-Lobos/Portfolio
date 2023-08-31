2a656caf5ca19f3b255c71b54237406445f02f03
Franco <francolobos3@gmail.com>
2023-08-31 22:38:07 +0200

    architectureFail

---
+++ src/components/Template/body/HomeBody.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { ProfileConst } from "../../../constants/ProfileConst";
+
+
+const HomeBody = ()=>{
+
+    const profile = ProfileConst.personals;
+    const languages = ProfileConst.languages;
+    const hobbies = ProfileConst.hobbies;
+    const job = ProfileConst.job;
+    const contact = ProfileConst.contact;
+
+    const [age, setAge] = useState(0);
+
+    const calculateAge = ()=>{
+        let now = new Date();
+        let birth = new Date(profile.birth);
+        let age = now-birth;
+        let year = 1000*60*60*24*365
+        age = Math.floor(age / year);
+        setAge(age);
+    };
+
+    useEffect(()=>{
+        calculateAge();
+    },[])
+
+
+    return(
+        <div>
+            <div id={"home-body-main"}>
+            <br/>
+                <span className="color-blue">const</span> <span className="color-light-blue">Personals</span> = <span className="color-purple">{`{`}</span>
+                <p style={{paddingLeft:"4rem", margin: "1rem"}}>
+                    <span className="color-light-blue"> fullName : </span> <span className="color-orange">{`"${profile.name} ${profile.lastName}"`}</span>, <br/>
+                    <span className="color-light-blue"> nationality : </span> <span className="color-orange">{`"${profile.nationality}"`}</span>, <br/>
+                    <span className="color-light-blue"> livesIn : </span> <span className="color-orange">{`"${profile.livesIn}"`}</span>, <br/>
+                    <span className="color-light-blue"> age : </span> <span className="color-green">{`${age}`}</span>, <br/>
+
+                    <span className="color-light-blue"> languages : </span> <span className="color-purple">{`[`}</span>
+                        <p style={{paddingLeft:"4rem"}}>
+                            {
+                                languages.map((lan, indx)=>
+                                    <span key={indx} >
+                                        <span className="color-blue">{`{`}</span>
+                                        <span className="color-light-blue"> name: </span> <span className="color-orange"> "{lan.name}"</span>,
+                                        <span className="color-light-blue"> &emsp; level:</span> <span className="color-orange"> "{lan.level}" </span>
+                                        <span className="color-blue">{`}`}</span>
+                                        {indx ===languages.length-1 ? "" : ","}
+                                        <br/>
+                                    </span>
+                                )
+                            }
+                        </p>
+                    <span className="color-purple">{`]`}</span>, <br/>
+                    <span className="color-light-blue"> hobbies : </span> <span className="color-purple">{`[`}</span>
+                        <p style={{paddingLeft:"4rem"}}>
+                            {hobbies.map((hob,indx)=>
+                            <>
+                                <span className="color-orange" key={indx}> "{hob}"</span>{indx ===hobbies.length-1 ? "" : ","}
+                            </>
+                            )}
+                        </p>
+                    <span className="color-purple">{`]`}</span>,<br/>
+        
+                    <span className="color-light-blue"> openToWork : </span> <span className="color-blue">{`${job.openToWork ? "true" :" false"}`}</span>, <br/>
+                    <span className="color-light-blue"> openReloacate : </span> <span className="color-blue">{`${job.openToRelocate ? "true" :" false"}`}</span>, <br/>
+                    <span className="color-light-blue"> contact : </span> <a className="color-orange" href={`mailto:${contact.mail}`} target="_blank"> "{contact.mail}"</a>, <br/>
+                    <span className="color-light-blue"> linkedIn : </span> <a className="color-orange" href={`${contact.linkedIn}`} target="_blank"> "{contact.linkedIn}"</a>, <br/>
+                    <span className="color-light-blue"> gitHub : </span> <a className="color-orange" href={`${contact.gitHub}`} target="_blank"> "{contact.gitHub}"</a>, <br/>
+
+                </p>
+                <span className="color-purple">{`}`}</span>;
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default HomeBody;
+
---
+++ src/constants/ProfileConst.jsx
@@ -0,0 +1,60 @@
+export const ProfileConst = {
+    personals:{
+        name:"Franco",
+        lastName: "Lobos",
+        nationality: "Argentino",
+        livesIn: "Andorra",
+        birth: "1996-01-19T09:05:00.000Z"
+    },
+
+    languages:[
+        {name: "Español", level: "Native"},
+        {name: "English", level: "B2"},
+        {name: "Català", level: "A1"},
+        {name: "Deutsch", level: "Ich lerne..."}
+
+    ],
+
+    contact:{
+        mail: "francolobos3@gmail.com",
+        pohone: "376649899",
+        linkedIn: 'https://www.linkedin.com/in/franco-lobos-7142269a/',
+        gitHub: 'https://github.com/Franco-Lobos',
+    },
+
+    job:{
+        openToWork :1,
+        openToRelocate :1,
+    },
+
+    hobbies: ["Chess", "Guitar", "RPG games", "Wine tasting", "Cooking", "Snowboard"],
+
+
+    intro:[`
+        About me?`, 
+        // 'Generic example creative example lorem ispun consolidated',
+        `I have always driven my gastronomic projects.
+        I also had my own restaurant, wine brand, and drinks brand. 
+        Therefore, I consider myself a very creative and enterprising person with adaptation skills.`,
+        `In 2021 I learned about the information technology world.
+        I took my first steps learning Python self-taught and working as a WordPress plugin developer
+        for a local company.`,
+        `During 2022, I immersed myself in learning React to improve my skills. I then had the opportunity to collaborate on various projects using this technology.`,
+        `Additionally, I decided to enroll in an online <aid="link-0"/> at UOC on June 2022. Meanwhile, I continued to work as a freelancer on different projects.`,
+        `My goal is to make coding a full-time job in a consolidated company.`
+    ],
+
+    specialWords:[
+        `creative`, `adaptation`, `python`, `wordpress`, `Bachelor's degree`, `consolidated`,
+        `bachelor's degree`, `univesity`, `react`
+    ],
+
+
+    links:[
+        {
+            id: 0,
+            text: "Bachelor's degree in Software Development",
+            href:"https://studies.uoc.edu/en/bachelors-degrees/software-development/study-plan"
+        }
+    ]
+}
