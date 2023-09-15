f2143e86d97f8bf44134b38def06d2e562123080
Franco <francolobos3@gmail.com>
2023-09-16 00:13:54 +0200

    introUpdate

--- src/constants/ProfileConst.jsx
+++ src/constants/ProfileConst.jsx
@@ -33,20 +33,24 @@ export const ProfileConst = {
     intro:[`
         About me?`, 
         // 'Generic example creative example lorem ispun consolidated',
-        `I have always driven my gastronomic projects.
-        I also had my own restaurant, wine brand, and drinks brand. 
-        Therefore, I consider myself a very creative and enterprising person with adaptation skills.`,
-        `In 2021 I learned about the information technology world.
-        I took my first steps learning Python self-taught and working as a WordPress plugin developer
-        for a local company.`,
-        `During 2022, I immersed myself in learning React to improve my skills. I then had the opportunity to collaborate on various projects using this technology.`,
-        `Additionally, I decided to enroll in an online <aid="link-0"/> at UOC on June 2022. Meanwhile, I continued to work as a freelancer on different projects.`,
-        `My goal is to make coding a full-time job in a consolidated company.`
+        // `I have always driven my gastronomic projects.
+        // I also had my own restaurant, wine brand, and drinks brand. 
+        // Therefore, I consider myself a very creative and enterprising person with adaptation skills.`,
+        // `In 2021 I learned about the information technology world.
+        // I took my first steps learning Python self-taught and working as a WordPress plugin developer
+        // for a local company.`,
+        // `During 2022, I immersed myself in learning React to improve my skills. I then had the opportunity to collaborate on various projects using this technology.`,
+        // `Additionally, I decided to enroll in an online <aid="link-0"/> at UOC on June 2022. Meanwhile, I continued to work as a freelancer on different projects.`,
+        // `My goal is to make coding a full-time job in a consolidated company.`
+
+        `As a highly skilled Front-End developer certified by Meta, I possess a strong knowledge of React.js, Node.js, JavaScript, HTML, and CSS. I am confident that I meet the requirements for a wide range of front-end projects, from completely interactive web applications to fully customizable websites.`,
+        `My experience also extends to server-side technologies, PHP included. I have demonstrated my proficiency through contributions to various WordPress projects, including personalized plugin development, shortcode creation, and collaboration on simple websites.`,
+        `In order to formalize my career as a software developer, I am pursing a <aid="link-0"/> at UOC. Currently, I am in the process of starting my third semester at this university, having completed 8 out of 29 courses.`,
+        `Carrying that experience in my toolkit, I'm presently in the process of establishing an Estonian company. My focus is on building enduring relationships with professional IT ` ,`I'd appreciate it if you could take a moment to visit this portfolio website to learn more about my background.`
     ],
 
     specialWords:[
-        `creative`, `adaptation`, `python`, `wordpress`, `Bachelor's degree`, `consolidated`,
-        `bachelor's degree`, `univesity`, `react`
+        `skilled`, `certified by meta`, `strong`, `interactive`, `php`, `university`, `react`, `personalized`, `javascript`, `software`, `developer`, `company`, `enduring`, `professional`
     ],
 
 
