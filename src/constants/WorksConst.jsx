5f2ec88f9c9913f998668e94db055e1deb8b05b0
Franco <francolobos3@gmail.com>
2024-08-26 22:21:34 +0200

    jobs update

--- src/constants/WorksConst.jsx
+++ src/constants/WorksConst.jsx
@@ -1,25 +1,47 @@
+
+// {
+//     name: `MyDRINKS repository`,
+//     description: `An Android .aab built in React and compiled using Apache Cordova`,
+//     link: `https://github.com/Franco-Lobos/myDrinks`,
+//     keyWords: [`Node.Js`, `React`, `Vs Code`, `CSS`, `Apache Cordova`],
+//     date: `27/06/22`,
+//     country: 'Andorra'
+// },
+
+
+// {
+//     name: `Baestic`,
+//     description: `WordPress web site for an IT company`,
+//     link: `https://baestic.com/`,
+//     keyWords: [`WordPress`, `CSS`],
+//     date: `08/09/22`,
+//     country: 'Andorra'
+// },
+
+
 export const WorksConst = {
-    specialWords:[
-        `PHP`, `Vanilla JS`,`Python`, `HTML`, `C`,
-        `CSS`, `Boostrap`,
-        `CodeLite`, `Vs Code`, `PhpStorm`,
+    specialWords: [
+        `PHP`, `Vanilla JS`, `Python`, `HTML`, `C`,
+        `CSS`, `Boostrap`, `Tailwind`
+            `CodeLite`, `Vs Code`, `PhpStorm`,
         `WordPress`,
-        `React`, `Node.Js`, `AJAX`,`Apache Cordova`, `P5.JS`,
-        `Symphony`,  `Git`, `Linux`, `Assambler`,  `Figma`,  `UX/UI`
+        `React`, `Node.Js`, `AJAX`, `Apache Cordova`, `P5.JS`,
+        `Symphony`, `Git`, `Linux`, `Assambler`, `Figma`, `UX/UI`, `Elementor`, `IA Prompting`,
+        `Java`, `MySQL`
     ],
 
-    proyects:[
+    proyects: [
         {
             name: `My company web site`,
             description: `Built in React, this website serves as my company's landing page.`,
             link: `https://klugerbyte.com/`,
-            keyWords: [`Git`, `React`,`Node.Js`,  `HTML`, `CSS`,  `Figma`, `UX/UI`],
+            keyWords: [`Git`, `React`, `Node.Js`, `HTML`, `CSS`, `Figma`, `UX/UI`],
             date: `03/08/23`,
             country: 'Andorra'
-        },        
-        
+        },
+
         {
-            name: `This web-page`,
+            name: `This web-page repository`,
             description: `This web page was built in React, using the P5.JS library to get the molecules interface done`,
             link: `https://github.com/franco-lobos/portfolio`,
             keyWords: [`Git`, `P5.JS`, `Node.Js`, `React`, `Vs Code`, `CSS`, `Vanilla JS`],
@@ -31,48 +53,40 @@ export const WorksConst = {
             name: `Pooling web demo`,
             description: `This design was created as a proposal for a design contest, and currently, only a few features have been implemented and are functional`,
             link: `https://main--poetic-daffodil-5c8119.netlify.app/`,
-            keyWords: [`Git`, `React`,`Node.Js`,  `HTML`, `CSS`],
+            keyWords: [`Git`, `React`, `Node.Js`, `HTML`, `CSS`, `Boostrap`],
             date: `08/09/23`,
             country: 'Andorra'
         },
-        
+
         {
-            name: `MyDRINKS`,
-            description: `An Android .aab built in React and compiled using Apache Cordova`,
-            link: `https://play.google.com/store/apps/details?id=my.drinks.ginWrold`,
-            keyWords: [`Node.Js`, `React`, `Vs Code`, `CSS`, `Apache Cordova`],
-            date: `27/06/22`,
+            name: `Prodigi Advisor`,
+            description: `WordPress page for a gestory company based in Andorra`,
+            link: `https://prodigiadvisor.com/`,
+            keyWords: [`WordPress`, `Elementor`, `UX/UI`, `IA Prompting`],
+            date: `09/06/24`,
             country: 'Andorra'
         },
 
         {
             name: `Maño Manitas`,
-            description: `WordPress page in wich, as a colaborator,  I added some Personalized shortcodes. `,
+            description: `WordPress page in wich, as a colaborator,  I developed some full-personalized shortcodes. `,
             link: `https://goodhandsmanitas.com/`,
-            keyWords: [`WordPress`,`PHP`, `HTML`, `CSS`],
+            keyWords: [`WordPress`, `PHP`, `HTML`, `CSS`],
             date: `08/09/22`,
             country: 'España'
         },
 
-        {
-            name: `Baestic`,
-            description: `WordPress web site for an IT company`,
-            link: `https://baestic.com/`,
-            keyWords: [`WordPress`,`CSS`],
-            date: `08/09/22`,
-            country: 'Andorra'
-        },
 
         {
             name: `Barbershop`,
             description: `A React web app with shopping cart and a backoffice panel`,
             link: `https://www.youtube.com/watch?v=1uv1puzJBnc`,
-            keyWords: [`Node.Js`, `React`, `Vs Code`, `CSS`, `Boostrap`],
+            keyWords: [`Node.Js`, `React`, `Vs Code`, `CSS`, `Tailwind`],
             date: `10/02/22`,
             country: 'República Dominicana'
         },
 
-         {
+        {
             name: `Brooklyn Quick Pay Money`,
             description: `Responsive Web site`,
             link: `https://www.youtube.com/watch?v=ZhgVjkhELEA`,
@@ -85,7 +99,7 @@ export const WorksConst = {
             name: `Qcero aberturas`,
             description: `Page in wich I implemented a client-customizable video slider plugin built from scratch, among other features.`,
             link: `https://qcero.com.ar/#carrousel-video-title-slide-container-gll`,
-            keyWords: [`WordPress`,`PHP`, `Vanilla JS`, `HTML`, `CSS`],
+            keyWords: [`WordPress`, `PHP`, `Vanilla JS`, `HTML`, `CSS`],
             date: `12/07/21`,
             country: 'Argentina'
         },
@@ -94,7 +108,7 @@ export const WorksConst = {
             name: `University traning`,
             description: `Relevant tecnologies studied in courses such as Operating Systems and Computer Structure`,
             link: `https://cv.uoc.edu/tren/trenacc/web/GAT_EXP.PLANDOCENTE?any_academico=20222&cod_asignatura=22.623&idioma=ANG&pagina=PD_PREV_PORTAL`,
-            keyWords: [`C`,`Linux`, `CodeLite`, `Assambler`],
+            keyWords: [`C`, `Java`, `Linux`, `CodeLite`, `Assambler`, `MySQL`],
             date: `08/09/22`,
             country: 'España'
         },
@@ -103,7 +117,7 @@ export const WorksConst = {
             name: `Self-training`,
             description: `Different and complementary self-taught skills from my first steps, for which I have not provided references.`,
             link: `https://github.com/Franco-Lobos/firstSteps`,
-            keyWords: [`Python`, `PhpStorm`,`AJAX`, `Symphony`],
+            keyWords: [`Python`, `PhpStorm`, `AJAX`, `Symphony`],
             date: `10/04/21`,
             country: 'Argentina'
         },
