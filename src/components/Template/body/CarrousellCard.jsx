27e60860a9f81b1514986dbd7f73b00c7fe412bc
Franco <francolobos3@gmail.com>
2024-12-21 19:42:14 +0100

    toucch event

--- src/components/Template/body/CarrousellCard.jsx
+++ src/components/Template/body/CarrousellCard.jsx
@@ -1,17 +1,19 @@
 
 const CarrousellCard = ({ card, indx, centered }) => {
 
+    const handleClick = () => {
+        window.open(card.link, '_blank');
+    }
+
     return (
-        <a className={`carrousell-card ${centered == indx ? ` active` : ``}`}
+        <div className={`carrousell-card ${centered == indx ? ` active` : ``}`}
             id={`carrousell-card-${indx}`}
-            href={card.link}
-            target="_blank"
-            onTouchStart={(e) => centered != indx ? e.preventDefault : ""}
-            onClick={(e) => centered != indx ? e.preventDefault : ""}>
+            onTouchStart={(e) => centered != indx ? e.preventDefault() : handleClick()}
+            onClick={(e) => centered != indx ? e.preventDefault() : handleClick()}>
             <div className="card-description" >{card.description}</div>
             <div className="card-date">{card.date}</div>
             <div className="card-country">{card.country}</div>
-        </a>
+        </div>
     );
 }
 
